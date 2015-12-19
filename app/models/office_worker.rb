c9c8028cef7f8cb121995553647986bad7dc6257
yunchang <yunchang@futureworkz.com>
2015-12-19 12:21:38 +0700

    Add  validate years_of_exp in OfficeWorker

--- app/models/office_worker.rb
+++ app/models/office_worker.rb
@@ -1,5 +1,7 @@
 class OfficeWorker < User
-  before_validation :age_greater_than_20
+  validates :years_of_exp, presence: true, numericality: {greater_than_or_equal_to: 0}
+
+  validate :age_greater_than_20
 
   private
   def age_greater_than_20
--- spec/models/office_worker_spec.rb
+++ spec/models/office_worker_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe OfficeWorker, type: :model do
+  context 'Validation' do
+    it { should validate_presence_of :years_of_exp }
+    it { should validate_numericality_of( :years_of_exp ).is_greater_than_or_equal_to(0) }
+  end
+
   describe '#Age must greater than 20' do
     let!(:office_worker) { FactoryGirl.build(:office_worker, dob: "29-04-2009") }
 
