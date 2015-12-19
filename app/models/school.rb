0a00bdee48a5772c1110cd5120e57852b084b83c
yunchang <yunchang@futureworkz.com>
2015-12-19 09:54:30 +0700

    Use enumerize

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,6 @@
 class School < ActiveRecord::Base
+  extend Enumerize
+
   validates  :name, :address, presence: true
-  
+  enumerize  :type_of_school, in: [:university, :high_school, :secondary_school, :primary_school], default: :primary_school
 end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe School, type: :model do
   context 'Validation' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :address }
+    it { should enumerize(:type_of_school).in(:university, :high_school, :secondary_school, :primary_school) }
   end
 
 end
